a809c5fa318f120feb400bdbf136bb974866a2e5
Eugen076 <166009796+Eugen076@users.noreply.github.com>
2024-05-25 02:35:04 +0300

    Add files via upload

---
+++ resource - Copie.h
@@ -0,0 +1,25 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by MFCApplication1.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_MFCAPPLICATION1_DIALOG      102
+#define IDR_MAINFRAME                   128
+#define IDC_BUTTON_SELECT_EXE           1004
+#define IDC_BUTTON_RUN_EXE              1005
+#define IDC_EDIT_OUTPUT                 1007
+#define IDC_EDIT_EXEPATH                1008
+#define IDC_BUTTON_EXIT                 1012
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1013
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
